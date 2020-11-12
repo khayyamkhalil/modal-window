01bec54d7a17110af1c4124c21c40c86c3ca56dc
Khayyam <65266683+khayyamkhalil@users.noreply.github.com>
2020-11-12 09:56:50 -0500

    added the functionality of hiding and showing modal window

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModal = document.querySelectorAll('.show-modal');
+
+// Created these functions for hiding/showing the modal window and overlay to reduce repetition.
+const showModal = function () {
+  //classList is used to manipulate with the html/css classes
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+// we used the for loop to looper over the Nodelist (querySelectorAll) to show all of the modals
+for (let i = 0; i < btnShowModal.length; i++) {
+  btnShowModal[i].addEventListener('click', showModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
